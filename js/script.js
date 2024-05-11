06e45dc3a2a9a4a5a6d80669c8ea80c0ce4de19c
Nicholas Chen <122904407+nicholalaaa@users.noreply.github.com>
2024-05-11 22:58:11 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let index=1;
+
+const moveTo = e => {
+    showSlide(index = e)
+}
+const changeSlide = e => {
+    showSlide(index += e)
+}
+
+const showSlide = e => {
+    const images = document.querySelectorAll('.slider-item');
+    const indicators = document.querySelectorAll('.slider-indicators');
+
+    (e > images.length) ? (index = 1) : null;
+    (e < 1) ? (index = images.length) : null;
+
+    for (let image of images) {
+        image.style.opacity= '0';
+    }
+     for (let indicator of indicators) {
+        indicator.style.width = '8px';
+        indicator.style.background = 'rgba(0,0,0,0)';
+     }
+
+     images[index - 1].style.opacity = '1';
+     indicators[index - 1].style.width = '26px';
+     indicator[index - 1].style.background = 'rgba(0,0,0,0)';
+}
+
+showSlide()

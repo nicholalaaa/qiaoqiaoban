07a34d49ca3d606ac064dae7a04943e72d0bcb80
Nicholas Chen <nc1020@ic.ac.uk>
2024-05-12 02:04:47 +0100

    adjust js

--- js/script4.js
+++ js/script4.js
@@ -1,30 +1,30 @@
-let index=1;
+let index4=1;
 
 const moveTo4 = e => {
-    showSlide4(index = e)
+    showSlide4(index4 = e)
 }
 const changeSlide4 = e => {
-    showSlide4(index += e)
+    showSlide4(index4 += e)
 }
 
 const showSlide4 = e => {
-    const images = document.querySelectorAll('.slider-item4');
-    const indicators = document.querySelectorAll('.slider-indicators4');
+    const images4 = document.querySelectorAll('.slider-item4');
+    const indicators4 = document.querySelectorAll('.slider-indicators4');
 
-    (e > images.length) ? (index = 1) : null;
-    (e < 1) ? (index = images.length) : null;
+    (e > images4.length) ? (index4 = 1) : null;
+    (e < 1) ? (index4 = images4.length) : null;
 
-    for (let image of images) {
-        image.style.opacity= '0';
+    for (let image4 of images4) {
+        image4.style.opacity= '0';
     }
-     for (let indicator of indicators) {
-        indicator.style.width = '8px';
-        indicator.style.background = 'rgba(0,0,0,0)';
+     for (let indicator4 of indicators4) {
+        indicator4.style.width = '8px';
+        indicator4.style.background = 'rgba(0,0,0,0)';
      }
 
-     images[index - 1].style.opacity = '1';
-     indicators[index - 1].style.width = '26px';
-     indicator[index - 1].style.background = 'rgba(0,0,0,0)';
+     images4[index4 - 1].style.opacity = '1';
+     indicators4[index4 - 1].style.width = '26px';
+     indicator4[index4 - 1].style.background = 'rgba(0,0,0,0)';
 }
 
 showSlide4()
